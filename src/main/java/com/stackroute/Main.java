97f5150f5aba0ab68c8daf5efc34b79b0f833b52
konda sraddha <k.sraddhareddy@gmail.com>
2019-07-15 10:36:50 +0530

    task4

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,48 +1,24 @@
 package com.stackroute;
 
-/*
-Create a Maven project and add required dependency of spring-context 5.1.4.RELEASE
-Create a Main class in package com.stackroute and two Spring Beans – Movie, and Actor in
-package com.stackroute.domain.
-Actor has two String properties, name and gender, and an age property of type int.
-An Actor can be initialized with the three properties via the corresponding setter methods. Use
-property based injection in the bean definition file (beans.xml)
-Movie “has a” Actor that can be initialized via the corresponding setter method. Use property
-based object injection in the bean definition file (beans.xml)
-The Main class looks up Movie bean via three ways to print out actor information:
-1. Using XmlBeanFactory
-2. Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader
-3. Using ApplicationContext
-Create a spring-xml-demo repo and push the code to master branch.
-
- */
-import com.stackroute.spring_demo.Movie;
 import com.stackroute.spring_demo.Actor;
-
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import com.stackroute.spring_demo.Movie;
+import org.springframework.beans.factory.BeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
 public class Main {
-
-    public  static  void main(String gopal []) {
-
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie=(Movie)applicationContext.getBean("movie2");
-        Movie movie2=(Movie)applicationContext.getBean("movie3");
-
-        System.out.println(movie.getActor());
-        System.out.println(movie2.getActor());
+    public static void main(String[] args) {
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        Movie m=(Movie)context.getBean("movie");
+        System.out.println(m.toString());
+        context.registerShutdownHook();
     }
 }
-/*
-Exception in thread "main" org.springframework.beans.factory.BeanCreationException:
- Error creating bean with name 'movie2' defined in class path resource [BeanFile.xml]:
-  Instantiation of bean failed; nested exception is org.springframework.beans.
-  BeanInstantiationException: Failed to instantiate [com.stackroute.domain.Movie]:
-  No default constructor found; nested exception is java.lang.NoSuchMethodException:
-  com.stackroute.domain.Movie.<init>()
-	at org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory.
-	instantiateBean(AbstractAutowireCapableBeanFactory.java:1287)
- */
--- src/main/java/com/stackroute/spring_demo/Movie.java
+++ src/main/java/com/stackroute/spring_demo/Movie.java
@@ -1,22 +1,58 @@
 package com.stackroute.spring_demo;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-    Actor actor;
+public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
+    private Actor actor;
+    ApplicationContext context;
+    private ApplicationContextAware applicationContextAware;
+    private  BeanFactoryAware beanFactoryAware;
+    private  BeanNameAware beanNameAware;
 
 
-    public Actor getActor() {
+    public ApplicationContext getContext() {
+        return context;
+    }
+
+    public void setContext(ApplicationContext context) {
+        this.context = context;
+    }
+
+    public Actor getActor()
+    {
         return actor;
     }
 
-    public void setActor(Actor actor) {
+    public void setActor(Actor actor)
+    {
+
         this.actor = actor;
+    }
+    @Override
+    public String toString()
+
+    {
+        return "Details : " + actor.toString();
     }
 
     @Override
-    public String toString() {
-        return "Movie{" +
-                "actor=" + actor +
-                '}';
+    public void setBeanName(String s) {
+        System.out.println("Bean name is set " +s);
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Set application context " +applicationContext);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactory "+beanFactory);
     }
 }
