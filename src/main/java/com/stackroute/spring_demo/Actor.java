2c6a7200d41250ccd4b1353e32f5d399db62dd3e
konda sraddha <k.sraddhareddy@gmail.com>
2019-07-15 10:46:58 +0530

    task6

--- src/main/java/com/stackroute/spring_demo/Actor.java
+++ src/main/java/com/stackroute/spring_demo/Actor.java
@@ -1,47 +1,43 @@
 package com.stackroute.spring_demo;
 
 public class Actor {
-
     private String name;
-    private  String gender;
-    private  int age;
+    private String gender;
+    private int age;
 
-    public String getName() {
-        return name;
-    }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
     }
 
     public void setGender(String gender) {
         this.gender = gender;
     }
 
-    public int getAge() {
-        return age;
-    }
-
     public void setAge(int age) {
         this.age = age;
     }
 
-    @Override
-    public String toString() {
-        return "Actor{" +
-                "name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", age=" + age +
-                '}';
+    public String getName() {
+        return name;
     }
 
-    public Actor(String name, String gender, int age) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public String toString() {
+        return "Name is : " + name + ", Gender is : " + gender + ", Age is : " + age;
+    }
+    public void initBean() {
+        System.out.println("Init Bean for : Actor");
+    }
+
+    public void destroyBean() {
+        System.out.println("Init Bean for : Actor");
     }
 }
---
+++ src/main/java/com/stackroute/spring_demo/BeanPostProcessorDemoBean.java
@@ -0,0 +1,19 @@
+package com.stackroute.spring_demo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Called postProcessBeforeInitialization() for :" + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Called postProcessBeforeInitialization() for :" + beanName);
+        return null;
+    }
+}
